ed68a0478e8d5ecc170cd6a422c9cd3188ef1943
yaowenqiang <yaowenqiang111@163.com>
2024-07-30 23:54:25 +0800

    add more refactor demos

--- session_FizzBuzz.py
+++ session_FizzBuzz.py
@@ -1,10 +1,55 @@
 def fizzbuzz(number):
-    for i in range(1, number + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            return "FizzBuzz"
-        elif i % 3 == 0:
-            return 'Fizz'
-        elif i % 5 == 0:
-            return "Buzz"
-        else:
-            return i
+    if number % 15 == 0:
+        return "FizzBuzz"
+    if number % 3 == 0:
+        return "Fizz"
+    if number % 5 == 0:
+        return 'Buzz'
+    return str(number)
+
+def fizzbuzz_v2(number):
+    result = ''
+    if number % 3 == 0:
+        result += "Fizz"
+    if number % 5 == 0:
+        result += "Buzz"
+    if result == '':
+        result = str(number)
+    return result
+
+def fizzbuzz_v3(number):
+    value = str()
+    if number % 3 == 0:
+        value += "Fizz"
+    if number % 5 == 0:
+        value += "Buzz"
+    return value or str(number)
+
+def divisible_by_5(number):
+    return number % 5 == 0
+    
+def divisible_by_3(number):
+    return number % 3 == 0
+
+def fizzbuzz_v4(number):
+    if divisible_by_3(number) and divisible_by_5(number):
+        return "FizzBuzz"
+    if divisible_by_3(number):
+        return "Fizz"
+    if divisible_by_5(number):
+        return "Buzz"
+    return str(number)
+
+def fizzbuzz_v4(number):
+    if number % 3 == 0 and number % 5 == 0: return "FizzBuzz"
+    if number % 3 == 0: return "Fizz"
+    if number % 5 == 0: return "Buzz"
+    return str(number)
+
+def fizzbuzz_v5(number):
+    fb_dict = {3:'Fizz', 5:'Buzz'}
+    result = str()
+    for key in fb_dict:
+        if number % key == 0:
+            result += fb_dict[key]
+    return result or str(number)
